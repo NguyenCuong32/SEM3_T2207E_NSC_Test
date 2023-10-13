4e3756150a17ad357b47e56bf3e28d0aec904b7e
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2023-10-13 18:51:25 +0700

    nộp bài

---
+++ Question1_1.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Create an instance of RSA.
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                // Generate keys
+                string publicKey = rsa.ToXmlString(false); // Public key
+                string privateKey = rsa.ToXmlString(true);  // Private key
+
+                // Get user input
+                Console.Write("Enter a message: ");
+                string plaintext = Console.ReadLine();
+
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(plaintext);
+                byte[] encryptedData = rsa.Encrypt(data, false);
+
+                // Decrypt data using the private key
+                byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+                string decryptedText = System.Text.Encoding.UTF8.GetString(decryptedData);
+
+                Console.WriteLine($"Original: {plaintext}");
+                Console.WriteLine($"Encrypted: {Convert.ToBase64String(encryptedData)}");
+                Console.WriteLine($"Decrypted: {decryptedText}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Provided username and password
+        string username = "Admin";
+        string password = "Awdx!@#$%xdwa";
+
+        // Encrypt the password using SHA-256
+        string hashedPassword = ComputeSHA256Hash(password);
+
+        Console.WriteLine($"Username: {username}");
+        Console.WriteLine($"Hashed Password: {hashedPassword}");
+    }
+
+    static string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
