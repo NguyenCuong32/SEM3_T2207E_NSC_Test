0a57e1601786fd8303474b7f214dcf1b7d230708
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-10-13 18:49:25 +0700

    UP

---
+++ SHA/SHA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string username = "Admin";
+        string password = "Awdx!@#$%xdwa";
+
+        string hashedPassword = ComputeSHA256Hash(password);
+
+        Console.WriteLine("Username: " + username);
+        Console.WriteLine("SHA-256 Hashed Password: " + hashedPassword);
+    }
+
+    public static string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
