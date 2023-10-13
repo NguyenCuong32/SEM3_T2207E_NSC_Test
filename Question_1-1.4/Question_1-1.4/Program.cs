4760b832cfb55b59d8c49b32a2e2b4e817fb3bd5
vudu151 <109599042+vudu151@users.noreply.github.com>
2023-10-13 19:06:53 +0700

    up code
    up code

---
+++ Question_1-1.4/Question_1-1.4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class AsymmetricEncryption
+{
+    // Mã hóa dữ liệu bằng public key
+    static byte[] EncryptData(string data, RSAParameters publicKey)
+    {
+        using (RSA rsa = RSA.Create())
+        {
+            rsa.ImportParameters(publicKey);
+            byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            byte[] encryptedData = rsa.Encrypt(dataBytes, RSAEncryptionPadding.Pkcs1);
+            return encryptedData;
+        }
+    }
+
+    // Giải mã dữ liệu bằng private key
+    static string DecryptData(byte[] encryptedData, RSAParameters privateKey)
+    {
+        using (RSA rsa = RSA.Create())
+        {
+            rsa.ImportParameters(privateKey);
+            byte[] decryptedBytes = rsa.Decrypt(encryptedData, RSAEncryptionPadding.Pkcs1);
+            string decryptedData = System.Text.Encoding.UTF8.GetString(decryptedBytes);
+            return decryptedData;
+        }
+    }
+
+    static void Main()
+    {
+        try
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                RSAParameters publicKey = rsa.ExportParameters(false);
+                RSAParameters privateKey = rsa.ExportParameters(true);
+
+                string text = "Hello world !";
+                Console.WriteLine($"Dữ liệu gốc: {text}");
+
+                byte[] encryptedData = EncryptData(text, publicKey); // Mã hóa dữ liệu bằng khóa công khai
+
+                string decryptedText = DecryptData(encryptedData, privateKey); // Giải mã dữ liệu bằng khóa riêng tư
+                Console.WriteLine($"Dữ liệu được giải mã: {decryptedText}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Question_3/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static string HashPassword(string inputPassword)
+    {
+        using(SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputPassword));
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < hashBytes.Length; i++)
+            {
+                stringBuilder.Append(hashBytes[i].ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+    }
+
+    static void Main()
+    {
+        string userName = "Admin";
+        string password = "Awdx!@#$%xdwa";
+        string hashPassword = HashPassword(password);
+        
+        Console.WriteLine("UserName: " + userName);
+        Console.WriteLine("Hash Password: " + hashPassword);
+    }
+}
