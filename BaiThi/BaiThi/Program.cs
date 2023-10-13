28af039e7de8bca8f5acc09c8e7dc02b0346ff9b
QuangThanh3001 <quangthanh33200@gmail.com>
2023-10-13 18:49:32 +0700

    nopbaithi

---
+++ BaiThi/BaiThi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string username = "Quản trị viên";
+        string password = "Awdx!@#$%xdwa";
+
+       
+        string hashedPassword = HashPassword(password);
+        
+        bool isAuthenticated = Authenticate(username, password, hashedPassword);
+
+        if (isAuthenticated)
+        {
+            Console.WriteLine("Đăng nhập thành công!");
+        }
+        else
+        {
+            Console.WriteLine("Đăng nhập không thành công!");
+        }
+    }
+
+    static string HashPassword(string password)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+         
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+         
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+
+    static bool Authenticate(string username, string password, string hashedPassword)
+    {
+
+        string hashedInputPassword = HashPassword(password);
+
+        return hashedInputPassword.Equals(hashedPassword);
+    }
+}
---
+++ Cau1.4/Cau1.4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            try
+            {
+                // Mã hóa
+                string publicKey = rsa.ToXmlString(false);
+                string privateKey = rsa.ToXmlString(true);
+
+                string name = "Nguyen Quang Thanh T2207E";
+                Console.WriteLine("Tên: " + name);
+
+                byte[] encryptedData = Encrypt(Encoding.UTF8.GetBytes(name), publicKey);
+                string encryptedText = Convert.ToBase64String(encryptedData);
+                Console.WriteLine("Mã hoá: " + encryptedText);
+
+                // Giải mã
+                byte[] decryptedData = Decrypt(Convert.FromBase64String(encryptedText), privateKey);
+                string maxuly = Encoding.UTF8.GetString(decryptedData);
+                Console.WriteLine("Giải mã: " + maxuly);
+            }
+            finally
+            {
+                rsa.PersistKeyInCsp = false;
+            }
+        }
+    }
+
+    static byte[] Encrypt(byte[] data, string publicKey)
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(publicKey);
+            return rsa.Encrypt(data, true);
+        }
+    }
+
+    static byte[] Decrypt(byte[] data, string privateKey)
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(privateKey);
+            return rsa.Decrypt(data, true);
+        }
+    }
+}
