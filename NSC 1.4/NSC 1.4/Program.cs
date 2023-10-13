d6f597dbd4fba8819be8ddeb5250fd62715fb518
tranbuithanh <109666928+tranbuithanh@users.noreply.github.com>
2023-10-13 19:00:51 +0700

    nopbai

---
+++ NSC 1.4/NSC 1.4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                string publicKey = rsa.ToXmlString(false);
+                string privateKey = rsa.ToXmlString(true);
+
+                Console.WriteLine("nhap van ban can ma hoa:");
+                string plainText = Console.ReadLine();
+
+                byte[] encryptedData = EncryptData(plainText, publicKey);
+
+                Console.WriteLine("van ban da ma hoa: " + Convert.ToBase64String(encryptedData));
+
+                string decryptedText = DecryptData(encryptedData, privateKey);
+
+                Console.WriteLine("van ban giai ma: " + decryptedText);
+            }
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine("loi: " + e.Message);
+        }
+    }
+
+    static byte[] EncryptData(string data, string publicKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(publicKey);
+            byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            byte[] encryptedData = rsa.Encrypt(dataBytes, false);
+            return encryptedData;
+        }
+    }
+
+    static string DecryptData(byte[] encryptedData, string privateKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(privateKey);
+            byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+            return System.Text.Encoding.UTF8.GetString(decryptedData);
+        }
+    }
+}
---
+++ NSC B3/NSC B3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        string combinedData = username + password;
+
+        string hashedPassword = CalculateSHA256Hash(combinedData);
+
+        Console.WriteLine("SHA-256 Hash of the password: " + hashedPassword);
+    }
+
+    static string CalculateSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+              return builder.ToString();
+        }
+    }
+}
