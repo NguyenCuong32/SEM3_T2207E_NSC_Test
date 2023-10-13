667194a0157136fd8e56c515428bde8eaa5e58c0
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2023-10-13 19:01:26 +0700

    nop bai test

---
+++ LamVanDuc/Cau3MaHoaChuoiVoiSHA256/Cau3MaHoaChuoiVoiSHA256/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using HashFunction.Hash;
+
+namespace HashFunction.Hash
+{
+    public class SHA1Hash
+    {
+        public static void Main()
+        {
+            Console.Write(" Moi ban nhap username: ");
+            string username = Console.ReadLine();
+            Console.Write(" Moi ban nhap password: ");
+            string text = Console.ReadLine();
+
+
+            string str_sha2 = SHA1Hash.SHA256(text);
+            Console.WriteLine($"username: {username}; password :{str_sha2}");
+            Console.ReadKey();
+        }
+  
+        public static string SHA256(string text)
+        {
+            SHA256Managed sha256 = new SHA256Managed();
+            StringBuilder hashSb = new StringBuilder();
+            byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+            foreach (byte b in hash)
+            {
+                hashSb.Append(b.ToString("x2"));
+            }
+            return hashSb.ToString();
+        }
+    }
+
+}
+
---
+++ LamVanDuc/GiaiMaBatDoiXung/GiaiMaBatDoiXung/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Tạo cặp khóa RSA (public key và private key)
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            // Lấy public key
+            string publicKey = rsa.ToXmlString(false);
+            Console.WriteLine($"Public Key:\n {publicKey}\n");
+
+            // Lấy private key
+            string privateKey = rsa.ToXmlString(true);
+            Console.WriteLine($"Private Key:\n {privateKey}\n");
+
+            // Chuỗi cần mã hóa
+            string tukhoa = "T2208E";
+
+            // Mã hóa trả về mảng byte , ta cần mảng byte để giải mã => convert mảng byte về dạng base 64 để xem được chuỗi đã mã hóa
+            byte[] encryptedData = RSAEncrypt(tukhoa, publicKey);
+
+            // Giải mã cho mảng byte đã mã hóa cùng với private key để giải mã
+            string decryptedData = RSADecrypt(encryptedData, privateKey);
+
+            // hiển thị 
+            Console.WriteLine($"Tu khoa: {tukhoa}");
+            Console.WriteLine($"Encrypted Data: {Convert.ToBase64String(encryptedData)}");
+            Console.WriteLine($"Decrypted Data: {decryptedData}");
+        }
+    }
+
+    // Hàm mã hóa RSA
+    static byte[] RSAEncrypt(string tukhoa, string publicKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            //sử dụng public key đã lấy từ trên để encrypt
+            rsa.FromXmlString(publicKey);
+            // chuyển văn bản cần encrypt sang dạng mảng byte
+            byte[] data = Encoding.UTF8.GetBytes(tukhoa);
+
+            return rsa.Encrypt(data, false);
+        }
+    }
+
+    // Hàm giải mã RSA
+    static string RSADecrypt(byte[] dataToDecrypt, string privateKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            // sử dụng private key để giải mã
+            rsa.FromXmlString(privateKey);
+
+            // chuyển văn bản đã mã hóa về dạng mảng byte
+            byte[] decryptedData = rsa.Decrypt(dataToDecrypt, false);
+
+            return Encoding.UTF8.GetString(decryptedData);
+        }
+    }
+}
+
