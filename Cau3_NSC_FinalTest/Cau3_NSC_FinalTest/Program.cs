17b69a04bd88a8f0514406be6ddc8d79abeb941e
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2023-10-13 18:49:47 +0700

    NgoMinhKhoi2603
    upTest

---
+++ Cau3_NSC_FinalTest/Cau3_NSC_FinalTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class PasswordEncryptionExample
+{
+    static void Main()
+    {
+        string username = "Admin";
+        string password = "Awdx!@#$%xdwa";
+
+        string hashedUsername = ComputeSHA256Hash(username);
+        string hashedPassword = ComputeSHA256Hash(password);
+
+        Console.WriteLine("Username: " + hashedUsername);
+        Console.WriteLine("Hashed Password: " + hashedPassword);
+        Console.ReadKey();
+    }
+
+    static string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ MaHoaVaGiaiMaBatDoiXung/MaHoaVaGiaiMaBatDoiXung/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            string plaintext;
+
+            Console.WriteLine("Nhập dữ liệu muốn mã hóa: ");
+            plaintext = Console.ReadLine();
+
+            byte[] data = Encoding.UTF8.GetBytes(plaintext);
+
+            byte[] encryptedData = rsa.Encrypt(data, false);
+
+            string encryptedText = Encoding.UTF8.GetString(encryptedData);
+
+            byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+
+            string decryptedText = Encoding.UTF8.GetString(decryptedData);
+
+            Console.WriteLine("Dữ liệu sau khi mã hóa: " + encryptedText);
+            Console.WriteLine("Dữ liệu sau khi giải mã: " + decryptedText);
+
+            Console.ReadKey();
+        }
+    }
+}
