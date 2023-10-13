e5232ee852be96e2c5f872831ecf2456420a2e6d
taonguyen2k <taonguyen01092000@gmail.com>
2023-10-13 19:00:22 +0700

    Test

---
+++ BT1/BT1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                
+                RSAParameters publicKey = rsa.ExportParameters(false); 
+                RSAParameters privateKey = rsa.ExportParameters(true);
+
+               
+                string originalData = "Hello, world!";
+                byte[] originalBytes = System.Text.Encoding.UTF8.GetBytes(originalData);
+                byte[] encryptedBytes = EncryptWithPublicKey(originalBytes, publicKey);
+
+
+                byte[] decryptedBytes = DecryptWithPrivateKey(encryptedBytes, privateKey);
+                string decryptedData = System.Text.Encoding.UTF8.GetString(decryptedBytes);
+
+
+                Console.WriteLine("Original Data: " + originalData);
+                Console.WriteLine("Encrypted Data: " + BitConverter.ToString(encryptedBytes).Replace("-", ""));
+                Console.WriteLine("Decrypted Data: " + decryptedData);
+            }
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine("An error occurred: " + e.Message);
+        }
+    }
+
+
+    static byte[] EncryptWithPublicKey(byte[] data, RSAParameters publicKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(publicKey);
+            return rsa.Encrypt(data, false);
+        }
+    }
+
+ 
+    static byte[] DecryptWithPrivateKey(byte[] data, RSAParameters privateKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(privateKey);
+            return rsa.Decrypt(data, false);
+        }
+    }
+}
+
+
+
+
---
+++ BT3/BT3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string username = "Admin";
+        string password = "Adwx!@#$%xdwa";
+
+        string salt = GenerateRandomSalt();
+
+       
+        string saltedPassword = password + salt;
+        string hashedPassword = ComputeSHA256Hash(saltedPassword);
+
+        Console.WriteLine("Username: " + username);
+        Console.WriteLine("Salt: " + salt);
+        Console.WriteLine("Hashed Password: " + hashedPassword);
+    }
+
+    static string GenerateRandomSalt()
+    {
+        byte[] saltBytes = new byte[16];
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(saltBytes);
+        }
+        return Convert.ToBase64String(saltBytes);
+    }
+
+    // Compute SHA-256 hash
+    static string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
